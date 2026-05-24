9e6582fa88cd2e61a6ef5f5e6e92660ae8bf3589
Mateus8637228 <Matrac2k20@gmail.com>
2026-05-25 00:30:11 +0700

    Начало положено

---
+++ Individual_project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Individual_project
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
